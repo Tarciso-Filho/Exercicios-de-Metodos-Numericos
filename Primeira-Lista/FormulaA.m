5919c34eeeb855e7c18a043ef950a09102b2dab4
Tarciso Filho <tarciso.filho@hotmail.com>
2019-12-05 18:46:20 -0300

    Criação de arquivos da Terceira Questão

---
+++ Primeira-Lista/FormulaA.m
@@ -0,0 +1,3 @@
+function formula1 = FormulaA(x0, y0, x1, y1)
+    formula1 = ( (x0 * y1) - (x1 * y0) ) / (y1 - y0)
+end
---
+++ Primeira-Lista/FormulaB.m
@@ -0,0 +1,3 @@
+function formula2 = FormulaB(x0, y0, x1, y1)
+    formula2 = x0 - ( ( (x0 - x1) * y0 ) / (y1 - y0) )
+end
