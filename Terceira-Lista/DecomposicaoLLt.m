7c216ffad7fc4f1a8c1e78e4845009629aa1fcd7
Tarciso Filho <tarciso.filho@hotmail.com>
2020-01-28 14:22:25 -0300

    Outros Arquivos da Primeira Questão

---
+++ Terceira-Lista/DecomposicaoLLt.m
@@ -0,0 +1,9 @@
+function raizes = DecomposicaoLLt( dimensao, coeficientes )
+    l = sqrt( coeficientes(1,1) )
+    for j = 2:n
+        l = [ l (coeficientes(j,1)) ]
+    end
+    for 2:(n-1)
+        
+    end
+end
---
+++ Terceira-Lista/MetodoDeCholesky.m
@@ -0,0 +1,8 @@
+%Quando houver erro, ou seja, se não for possível calcular as raizes,
+%NaN(size(Entrada,1)).
+%   A=[1 3 4; 2 4 5]
+%   A([1 2],:)=A([2 1],:)
+
+function raizes = MetodoDeCholesky( sistema )
+    
+end
