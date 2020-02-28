e169c2fc71a086b89389eff731f2c7002f4485a9
Tarciso Filho <tarciso.filho@hotmail.com>
2020-02-28 00:24:31 -0300

    Resposta A da Segunda Questão
    Tentativa de arquivo pequeno para resolução

---
+++ Quarta-Lista/MetodoDeGaussSeideiA.m
@@ -0,0 +1,7 @@
+function raizes = gauss_seidel(A, b, raizes, n)
+    for i = 1:n
+        for j = 1:size(A,1)
+            raizes(j) = (1/A(j,j)) * (b(j) - A(j,:)*raizes + A(j,j)*raizes(j));
+        end
+    end
+end
