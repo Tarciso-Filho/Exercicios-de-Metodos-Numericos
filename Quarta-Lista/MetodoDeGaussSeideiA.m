2df0a86ebb743fd1cd075f020e1b1636a5ca45a0
Tarciso Filho <tarciso.filho@hotmail.com>
2020-02-28 00:28:25 -0300

    Ainda na segunda Questão

--- Quarta-Lista/MetodoDeGaussSeideiA.m
+++ Quarta-Lista/MetodoDeGaussSeideiA.m
@@ -1,4 +1,4 @@
-function raizes = gauss_seidel(A, b, raizes, n)
+function raizes = MetodoDeGaussSeideiA(A, b, raizes, n)
     for i = 1:n
         for j = 1:size(A,1)
             raizes(j) = (1/A(j,j)) * (b(j) - A(j,:)*raizes + A(j,j)*raizes(j));
